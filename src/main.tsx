3ddb11a885d91328e38d28a05311018aaf9ebf2c
Nguyễn Tân Tiến <chaotien2003@gmail.com>
2025-03-11 16:13:18 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './styles/index.css'
 import App from './routes/App'
 
-// Create a client
+
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
